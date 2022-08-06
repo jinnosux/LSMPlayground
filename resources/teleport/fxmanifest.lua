3dfca5b2ce35f9e4282c2416c570f44c9936f59b
Fetu <110632602+YakuzaLSM@users.noreply.github.com>
2022-08-06 14:52:54 +0300

    Update fxmanifest.lua
    fixed

--- resources/teleport/fxmanifest.lua
+++ resources/teleport/fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 author 'Fetu'
-decription 'Teleport to another player with /teleport [player id] or [playername]. Also you can teleport player to you with /getplayer'
+decription 'Teleport to another player with /tp [player id]. Also you can teleport player to you with /getplayer [player id].'
 
 server_scripts {
     'resources/teleport/server.lua',
