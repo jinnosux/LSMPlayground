a6d428e3385008ce17f883ebca4766895032fb8f
jinnosux <vahid.k@ibu.edu.mk>
2022-08-06 16:07:00 +0200

    fix for loa load

--- resources/teleport/fxmanifest.lua
+++ resources/teleport/fxmanifest.lua
@@ -4,9 +4,9 @@ author 'Fetu'
 decription 'Teleport to another player with /tp [player id]. Also you can teleport player to you with /getplayer [player id].'
 
 server_scripts {
-    'resources/teleport/server.lua',
+    'server.lua',
 }
 
 client_scripts {
-    'resources/teleport/client.lua',
+    'client.lua',
 }
