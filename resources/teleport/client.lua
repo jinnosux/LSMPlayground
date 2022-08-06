e553a18dcec7699eb7c86a3e589de23920eaa2c4
Ferhat Arslan <110632602+YakuzaLSM@users.noreply.github.com>
2022-08-06 16:57:01 +0300

    .
    .

--- resources/teleport/client.lua
+++ resources/teleport/client.lua
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 -- Command to go to another player
 RegisterCommand('tp', function(_, args)
     local targetId = args[1]
@@ -27,3 +28,34 @@ RegisterCommand('getplayer', function(_, args)
 
     TriggerServerEvent('ch_teleporter:getplayer', targetId)
 end)
+=======
+-- Command to go to another player
+RegisterCommand('tp', function(_, args)
+    local targetId = args[1]
+
+    if not targetId then
+        TriggerEvent('chat:addMessage', {
+            args = { 'Please provide a target ID.', },
+        })
+
+        return
+    end
+
+    TriggerServerEvent('ch_teleporter:tp', targetId)
+end)
+
+-- Command to bring a player to us
+RegisterCommand('getplayer', function(_, args)
+    local targetId = args[1]
+
+    if not targetId then
+        TriggerEvent('chat:addMessage', {
+            args = { 'Please provide a target ID.', },
+        })
+
+        return
+    end
+
+    TriggerServerEvent('ch_teleporter:getplayer', targetId)
+end)
+>>>>>>> Stashed changes
--- resources/teleport/fxmanifest.lua
+++ resources/teleport/fxmanifest.lua
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 fx_version 'cerulean'
 game 'gta5'
 author 'Fetu'
@@ -10,3 +11,17 @@ server_scripts {
 client_scripts {
     'resources/teleport/client.lua',
 }
+=======
+fx_version 'cerulean'
+game 'gta5'
+author 'Fetu'
+decription 'Teleport to another player with /tp [player id]. Also you can teleport player to you with /getplayer [player id]'
+
+server_scripts {
+    'resources/teleport/server.lua',
+}
+
+client_scripts {
+    'resources/teleport/client.lua',
+}
+>>>>>>> Stashed changes
