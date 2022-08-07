fa760d462e4e866106167f4d7a89b70961bec743
Fetu <110632602+YakuzaLSM@users.noreply.github.com>
2022-08-07 18:42:06 +0300

    Update fxmanifest.lua

--- resources/teleport/fxmanifest.lua
+++ resources/teleport/fxmanifest.lua
@@ -4,10 +4,7 @@ lua54 'yes'
 author 'Fetu'
 decription 'Teleport to another player with /tp [player id]. Also you can teleport player to you with /getplayer [player id].'
 
-server_script {
-    'server.lua'
-}
+server_script 'server.lua'
 
-client_script {
-    'client.lua'
-}
+client_script 'client.lua'
+
