718252a7d5f662c9d1a9c7eb267290a43b525627
Fetu <110632602+YakuzaLSM@users.noreply.github.com>
2022-08-09 00:50:41 +0300

    fixed

--- resources/teleport_waypoint/main.lua
+++ resources/teleport_waypoint/main.lua
@@ -3,12 +3,7 @@ RegisterCommand('tpw', function(source, args)
     local src = source
     local waypoint = GetFirstBlipInfoId(8)
     local playerName = GetPlayerName(src)
-
-    local z = 10000<spawning ped> 
-    local PedHeight = natives.ENTITY.GET_ENTITY_HEIGHT_ABOVE_GROUND(PedID)
-    local z = z - PedHeight  
-    natives.ENTITY.SET_ENTITY_COORDS_NO_OFFSET(PedHeight,x,y,z + 3, true, true, true);
-
+ 
     if DoesBlipExist(waypoint) then
         SetEntityCoords(PlayerPedId(), GetBlipInfoIdCoord(waypoint), GetGroundZFor_3dCoord(x, y, z, false)) -- 'false' Means you don't teleport directly in the water.
         notify("You have just been teleported.")
