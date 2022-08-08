605c462132ba1f87dfe949ef8e6d9344cfcf076e
Fetu <110632602+YakuzaLSM@users.noreply.github.com>
2022-08-08 11:00:21 +0300

    fix

--- resources/teleport/server.lua
+++ resources/teleport/server.lua
@@ -21,6 +21,14 @@ end)
 RegisterNetEvent('ch_teleporter:getplayer', function(targetId)
     local playerId  = source
     local playerPed = GetPlayerPed(playerId)
+
+    if targetPed <= 0 then
+        TriggerClientEvent('chat:addMessage', playerId, {
+            args = { 'Player ' .. targetId .. ' doesn\'t seem to exist.', },
+        })
+        return
+    end
+
     local playerPos = GetEntityCoords(playerPed)
 
     TriggerClientEvent('ch_teleporter:teleport', targetId, playerPos)
